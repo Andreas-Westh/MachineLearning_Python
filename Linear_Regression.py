3f0856cd5132514c3cb0e8b14527c43949f69664
60gCreatine <cphah643@gmail.com>
2025-03-17 19:08:40 +0100

    plot and transfered to Jupyter

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -4,5 +4,16 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv("ecommerce.csv")
-df.head()
+# Makes it so it shows the full df
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width",None)
+df.head() 
 
+df.info() # Gets us the column name and other general info
+
+df.describe() # this is like summary() in R
+
+
+# EDA - Exploratory Data Analysis
+sns.jointplot(x="Time on Website", y="Yearly Amount Spent", data=df)
+plt.show()
